f1ff1d49a258a0ee66987615b6431d70425f4b49
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-09-27 11:21:27 +0100

    added css to weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div class="container">
-        Weather App
+        <Weather />
         <footer>
           This project was cofed by Vicki Seaman and is {""}
           <a
