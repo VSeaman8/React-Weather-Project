19108498abb616f99132bb532c869bf13e473365
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-09-27 09:41:44 +0100

    added github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was cofed by Vicki Seaman and is {""}
+        <a
+          href="https://github.com/VSeaman8/React-Weather-Project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
