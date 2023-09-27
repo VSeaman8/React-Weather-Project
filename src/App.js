034e24d9a6e7d510a74671a72f9d1070e9c12f76
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-09-27 09:55:24 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was cofed by Vicki Seaman and is {""}
-        <a
-          href="https://github.com/VSeaman8/React-Weather-Project"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div class="container">
+        Weather App
+        <footer>
+          This project was cofed by Vicki Seaman and is {""}
+          <a
+            href="https://github.com/VSeaman8/React-Weather-Project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
