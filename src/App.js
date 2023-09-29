1011b6728d589d553e24bfe97fbdd5058ca3dab9
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2023-09-29 08:58:52 +0100

    corrected class to classname in app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <Weather defaultCity="Birmingham" />
+
         <footer>
-          This project was cofed by Vicki Seaman and is {""}
+          This project was coded by Vicki Seaman and is {""}
           <a
             href="https://github.com/VSeaman8/React-Weather-Project"
             target="_blank"
